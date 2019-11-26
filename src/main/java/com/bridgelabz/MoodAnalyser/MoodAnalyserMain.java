d9c90099122fd02daa4e6a35f8c2fd882c27bebe
Sheetal Bedarkar <sheetalbedarkar96@gmail.com>
2019-11-26 11:24:24 +0530

    Refactor code to take mood message in contructor

--- src/main/java/com/bridgelabz/MoodAnalyser/MoodAnalyserMain.java
+++ src/main/java/com/bridgelabz/MoodAnalyser/MoodAnalyserMain.java
@@ -1,7 +1,21 @@
 package com.bridgelabz.MoodAnalyser;
 
 public class MoodAnalyserMain {
+    String message;
+
+    public MoodAnalyserMain() {
+    }
+
+    public MoodAnalyserMain(String message) {
+        this.message = message;
+    }
+
     public String analyseMood(String message) {
+        this.message = message;
+        return analyseMood();
+    }
+
+    public String analyseMood() {
         if(message.contains("SAD")) {
             return "SAD";
         }
--- src/test/java/com/bridgelabz/MoodAnalyserTest/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest/MoodAnalyserTest.java
@@ -5,17 +5,16 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
+    MoodAnalyserMain moodAnalyser = new MoodAnalyserMain(null);
     @Test
     public void givenMessage_whenSad_ShowReturnSad() {
-        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain();
-        String mood = moodAnalyser.analyseMood("SAD");
+        String mood = moodAnalyser.analyseMood("This is SAD mood");
         Assert.assertEquals("SAD",mood);
     }
 
     @Test
     public void givenMessage_whenHappy_ShowReturnHappy() {
-        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain();
-        String mood = moodAnalyser.analyseMood("HAPPY");
+        String mood = moodAnalyser.analyseMood("This is HAPPY mood");
         Assert.assertEquals("HAPPY",mood);
     }
 }
